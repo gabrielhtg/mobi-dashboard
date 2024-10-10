e39105cadd62394af0f529405ca642901d6f9b6c
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-10-10 17:18:40 +0700

    new ui like mobitech app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# GDashboardApp
+# Mobi Dashboard
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.7.
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./node_modules/preline/dist/preline.js",
-    "./src/**/*.{html,ts}"
-  ],
+  darkMode: "class",
+  content: ["./node_modules/preline/dist/preline.js", "./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sidebarBlue: "#d8e9f6",
+        navbarBlue: "#0d75bc",
+      },
+    },
   },
-  plugins: [
-    require('preline/plugin'),
-  ],
-}
-
+  plugins: [require("preline/plugin")],
+};
