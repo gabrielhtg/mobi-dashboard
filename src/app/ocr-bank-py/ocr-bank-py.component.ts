8a408f44d41d349fe821470ab3092da42d244cee
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-10-23 10:53:58 +0700

    mengganti routing

--- src/app/ocr-bank-py/ocr-bank-py.component.ts
+++ src/app/ocr-bank-py/ocr-bank-py.component.ts
@@ -67,27 +67,7 @@ export class OcrBankPyComponent implements AfterViewInit {
           formData.append('zip-password', this.zipPassword!);
         }
 
-        this.http.get<any>('assets/response-bca-corp.json').subscribe({
-          next: (value) => {
-            Swal.close();
-
-            this.router
-              .navigate(['/dashboard/ocr-bca-result'], {
-                state: value,
-              })
-              .then();
-          },
-          error: (err) => {
-            Swal.fire({
-              icon: 'error',
-              title: 'Upload Failed',
-              text:
-                err.error.data == undefined ? 'Unknown Error!' : err.error.data, // Bisa disesuaikan dengan pesan yang lebih jelas
-            });
-          },
-        });
-
-        // this.http.post<any>(`${apiUrlPy}/proceed-bca`, formData).subscribe({
+        // this.http.get<any>('assets/response-bca-corp.json').subscribe({
         //   next: (value) => {
         //     Swal.close();
 
@@ -106,6 +86,26 @@ export class OcrBankPyComponent implements AfterViewInit {
         //     });
         //   },
         // });
+
+        this.http.post<any>(`${apiUrlPy}/proceed-bca`, formData).subscribe({
+          next: (value) => {
+            Swal.close();
+
+            this.router
+              .navigate(['/dashboard/ocr-bca-result'], {
+                state: value,
+              })
+              .then();
+          },
+          error: (err) => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Upload Failed',
+              text:
+                err.error.data == undefined ? 'Unknown Error!' : err.error.data, // Bisa disesuaikan dengan pesan yang lebih jelas
+            });
+          },
+        });
       }
 
       if (this.selectedBankStatement == '3') {
